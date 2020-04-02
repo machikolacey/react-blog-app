42023207f0a3679fad6091135c8717c49414eacb
Machiko Lacey <machiko_kimura_lacey@yahoo.co.uk>
2020-04-02 22:17:36 +0100

    Updates

---
+++ precache-manifest.823042c6df44f6d215d51de2b37b38e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd41d67da90b5861073a33c685db8694",
+    "url": "/react-blog-app/index.html"
+  },
+  {
+    "revision": "1b3322a7b6dcbd11c0fc",
+    "url": "/react-blog-app/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d70d3c33c93fd7603af1",
+    "url": "/react-blog-app/static/css/main.53ffc30f.chunk.css"
+  },
+  {
+    "revision": "1b3322a7b6dcbd11c0fc",
+    "url": "/react-blog-app/static/js/2.3f60ec32.chunk.js"
+  },
+  {
+    "revision": "abd7d8a29543e2ae2a09fbc633c1f289",
+    "url": "/react-blog-app/static/js/2.3f60ec32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70d3c33c93fd7603af1",
+    "url": "/react-blog-app/static/js/main.9eb6e315.chunk.js"
+  },
+  {
+    "revision": "117425faf7a4878d6c19",
+    "url": "/react-blog-app/static/js/runtime-main.31953137.js"
+  },
+  {
+    "revision": "6022c4d42538514b5f8c5faea493f67e",
+    "url": "/react-blog-app/static/media/_resized_1280.6022c4d4.jpg"
+  },
+  {
+    "revision": "353f8ae31951d704d3aef38a89df5dc6",
+    "url": "/react-blog-app/static/media/_resized_300.353f8ae3.jpg"
+  },
+  {
+    "revision": "59595eb8b63e36321c3bdfd11fbeb38e",
+    "url": "/react-blog-app/static/media/_resized_768.59595eb8.jpg"
+  }
+]);
